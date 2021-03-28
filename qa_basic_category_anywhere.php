15a518936fc23f33e32610c9ebcd84e60709e9f7
ZhaoGuangYue1986 <jeky_zhao@.com>
2021-03-28 17:12:06 +0800

    支持用户定义显示数量，显示分类及独立CSS

--- qa_basic_category_anywhere.php
+++ qa_basic_category_anywhere.php
@@ -22,11 +22,49 @@
 
 class qa_basic_category_anywhere
 {
+	private $has_echo_cat_num=0;
+	
 	public function allow_template($template)
 	{
 		return $template != 'admin';
 	}
 
+	public function option_default($option)
+	{
+		switch ($option) {
+			case 'is_show_sub_category':
+				return false;
+			case 'max_category_to_show':	
+			    return 10;
+			case 'which_category_to_show':	
+			    return '';
+			case 'header_css_style':	
+			    return '.qa_cat_head_self_def{
+						padding: 10px;
+						background: #9b59b7;
+						color: #fff;
+						margin: 0 0 5px 0;
+						font-size: 20px;
+						font-weight:800;
+						}';
+			case 'category_list_css_style':	
+			    return '.qa_cat_link_self_def {
+							background-color: #c0392b;
+							margin: 0 0 5px 0;
+							font-size: 15px;
+							font-weight: 500;
+							color: #fff;
+							padding: 2px 1em;
+							display: inline-block;
+						}
+						.qa_ul_link_self_def {
+						}
+						.qa_ul_link_self_def a :hover,.qa_ul_link_self_def a:active,.qa_ul_link_self_def a:visited{
+							color: #fff;
+							border-bottom: 1px dotted #fff;
+						}';
+		}
+	}
 
 	public function allow_region($region)
 	{
@@ -37,24 +75,62 @@ class qa_basic_category_anywhere
 	public function admin_form()
 	{
 		$saved = false;
-		if (qa_clicked('save_button')) {
+		if (qa_clicked('save_change')) {
+			qa_opt('is_show_sub_category',(int)qa_post_text('is_show_sub_category'));
+			qa_opt('max_category_to_show', (int)qa_post_text('max_category_to_show'));
+			qa_opt('which_category_to_show', qa_post_text('which_category_to_show'));
+			qa_opt('header_css_style', qa_post_text('header_css_style'));
+			qa_opt('category_list_css_style', qa_post_text('category_list_css_style'));
 			$saved = true;
 		}elseif (qa_clicked('donate_zhao_guangyue')) {
 				qa_redirect_raw('https://paypal.me/guangyuezhao');
 		}
 
-
-
 		$form = array(
 			'ok' => $saved ? 'All oprations have been saved' : null,
 			'buttons' => array(
 				array(
-						'label' => 'Donate',
-						'tags' => 'NAME="donate_zhao_guangyue"',
+					'label' => 'Save',
+					'tags' => 'name="save_change"',
+				),array(
+					'label' => 'Donate',
+					'tags' => 'NAME="donate_zhao_guangyue"',
 				)
 			),
 			'fields' => array(
-				array(
+				'is_show_sub_category'=>array(
+					'label' => 'Is show sub category',
+					'type' => 'checkbox',
+					'value' => (int)qa_opt('is_show_sub_category'),
+					'tags' => 'name="is_show_sub_category"',
+			    ),
+			    'max_category_to_show'=>array(
+					'label' => 'max category to show 0 means not limit:',
+					'tags' => 'NAME="max_category_to_show"',
+					'value' => (int)qa_opt('max_category_to_show'),
+					'type' => 'number',
+				),
+			    'which_category_to_show'=>array(
+					'label' => 'Which category to show,separate category name with commas, empty for all (e.g AA,BB):',
+					'tags' => 'NAME="which_category_to_show"',
+					'value' => qa_opt('which_category_to_show'),
+					'type' => 'text',
+				),
+			    'header_css_style'=>array(
+					'label' => 'Css style for category header (Do not change css name [ qa_cat_head_self_def ] it was used in php code):',
+					'tags' => 'NAME="header_css_style"',
+					'value' => qa_opt('header_css_style'),
+					'rows' => 10,
+					'type' => 'textarea',
+				),
+			    'category_list_css_style'=>array(
+					'label' => 'Css style for category list (Do not change css name [ qa_cat_link_self_def ] it was used in php code):',
+					'tags' => 'NAME="category_list_css_style"',
+					'value' => qa_opt('category_list_css_style'),
+					'rows' => 10,
+					'type' => 'textarea',
+				),
+				'hope_donate'=>array(
 					'label' => '<span style="color:#f90; font-size:16px; text-align:center;">Hope you can donate <strong>$1</strong> for my work!</br> Thanks very much!</span>',
 					'type' => 'custom',
 					'tags' => 'NAME="hope_donate"',
@@ -65,8 +141,9 @@ class qa_basic_category_anywhere
 	}
 	
 	public function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
-	{
+	{   
 		$this->themeobject = $themeobject;
+		$this->add_self_def_style($themeobject);
 		if (isset($qa_content['navigation']['cat'])) {
 			$nav = $qa_content['navigation']['cat'];
 		} else {
@@ -75,52 +152,75 @@ class qa_basic_category_anywhere
 			$nav = qa_category_navigation($navcategories);
 		}
 		
-		$this->themeobject->output('<h2>' . qa_lang_html('main/nav_categories') . ' </h2>');
-		$this->nav_list($themeobject,$nav, 'browse-cat', 0);
+		$this->themeobject->output('<h2 class="qa_cat_head_self_def">' . qa_lang_html('main/nav_categories') . ' </h2>');
+		$this->nav_list($themeobject,$nav, 0);
 		$this->themeobject->output('</br>');
 	}
 	
-	public function nav_list($outPutCtent,$navigation, $class, $level = null)
+	public function nav_list($outPutCtent,$navigation, $level = null)
 	{
-		$outPutCtent->output('<ul class="qa-' . $class . '-list' . (isset($level) ? (' qa-' . $class . '-list-' . $level) : '') . '">');
-		
+		$this->has_echo_cat_num = 0;
+		$outPutCtent->output('<ul class="qa_ul_link_self_def qa-browse-cat-list' . (isset($level) ? (' qa-browse-cat-list-' . $level) : '') . '">');
 		foreach ($navigation as $key => $navlink) {
-			$this->nav_item($outPutCtent,$key, $navlink, $class, $level);
+			$this->nav_item($outPutCtent, $navlink, $level);
 		}
 		
 		$outPutCtent->output('</ul>');
 	}
 	
-	public function nav_item($outPutCtent,$key, $navlink, $class, $level = null)
+	public function nav_item($outPutCtent, $navlink, $level = null)
 	{
-		$suffix = strtr($key, array( // map special character in navigation key
-			'$' => '',
-			'/' => '-',
-		));
-
-		$this->nav_link($outPutCtent,$navlink, $class);
+		$this->nav_link($outPutCtent,$navlink);
 
 		$subnav = isset($navlink['subnav']) ? $navlink['subnav'] : array();
- 		if (is_array($subnav) && count($subnav) > 0) {
+ 		if (is_array($subnav) && count($subnav) > 0 && qa_opt('is_show_sub_category')) {
 			foreach ($subnav as $key => $navlink) {
-				$this->nav_item($outPutCtent,$key, $navlink, $class, $level);
+				$this->nav_item($outPutCtent,$navlink, $level);
 			}
 		}
 	}
 	
-	public function nav_link($outPutCtent,$navlink, $class)
+	public function nav_link($outPutCtent,$navlink)
 	{
-		if (isset($navlink['url'])) {
-			$statNum = strlen(@$navlink['note']) > 0 ? $navlink['note']:'';
-			$outPutCtent->output(
-				'<a href="' . $navlink['url'] . '" class="qa-' . $class . '-link ">' .$navlink['label'] ." ".$statNum.'</a>'
-			);
-		} else {
-			$outPutCtent->output(
-				'<span class="qa-' . $class . '-nolink' . (@$navlink['selected'] ? (' qa-' . $class . '-selected') : '') .
-				(strlen(@$navlink['popup']) ? (' title="' . $navlink['popup'] . '"') : '') .
-				'>' . $navlink['label'] . '</span>'
-			);
-		} 
+		//echo qa_opt('max_category_to_show');
+		$max_category_to_show = qa_opt('max_category_to_show');
+		if($max_category_to_show == 0 || $this->has_echo_cat_num < $max_category_to_show){
+			if (isset($navlink['url'])) {
+				if($this->is_cat_in_user_def($navlink['label'])){
+					$statNum = strlen(@$navlink['note']) > 0 ? $navlink['note']:'';
+					$outPutCtent->output(
+						'<a href="' . $navlink['url'] . '" class="qa_cat_link_self_def ">' .$navlink['label']." ".$statNum.'</a>'
+					);
+					$this->has_echo_cat_num = $this->has_echo_cat_num + 1;	
+				}
+			} else {
+				$outPutCtent->output(
+					'<span class="qa-' . $class . '-nolink' . (@$navlink['selected'] ? (' qa-' . $class . '-selected') : '') .
+					(strlen(@$navlink['popup']) ? (' title="' . $navlink['popup'] . '"') : '') .
+					'>' . $navlink['label'] . '</span>'
+				);
+				$this->has_echo_cat_num = $this->has_echo_cat_num + 1;	
+			}
+            		
+		}
+	}
+	
+	public function add_self_def_style($outPutCtent){
+		$outPutCtent->output('<style>',qa_opt('header_css_style'),'</style>');
+		$outPutCtent->output('<style>',qa_opt('category_list_css_style'),'</style>');
+	}
+	
+	public function is_cat_in_user_def($cat_name){
+	    if(trim(qa_opt('which_category_to_show')=='')){
+           return true;
+		}else{
+			$user_show_cat = explode(',',qa_opt('which_category_to_show'));	
+			foreach($user_show_cat as $value){
+				if(trim($value)==$cat_name){
+					return true;
+				}
+			}
+			return false;
+		}
 	}
 }
